48d1e58ea3841855922bbdc5525b628a26700acf
a1104004650 <1104004650@qq.com>
2022-03-03 20:26:02 +0800

    Initial commit

---
+++ src/test/java/com/zbc/practise/PractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zbc.practise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PractiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
