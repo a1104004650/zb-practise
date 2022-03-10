927dff0baf98e5cfd1e2312f6c659eb86418ddca
a1104004650 <1104004650@qq.com>
2022-03-10 13:53:59 +0800

    添加注释

--- src/main/java/com/zbc/practise/thread/BaseService.java
+++ src/main/java/com/zbc/practise/thread/BaseService.java
@@ -7,6 +7,11 @@ import java.util.concurrent.Executors;
 
 /**
  * @author StormT1King
+ * 线程池不允许使用Executors去创建，而是通过ThreadPoolExecutor的方式，这样的处理方式让写的同学更加明确线程池的运行规则，规避资源耗尽的风险。 说明：Executors返回的线程池对象的弊端如下：
+ * 1）FixedThreadPool和SingleThreadPool:
+ * 允许的请求队列长度为Integer.MAX_VALUE，可能会堆积大量的请求，从而导致OOM。
+ * 2）CachedThreadPool:
+ * 允许的创建线程数量为Integer.MAX_VALUE，可能会创建大量的线程，从而导致OOM。
  */
 public class BaseService {
 
